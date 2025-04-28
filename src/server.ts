65fcd7eed94887bbe07c5097f361a7f356450677
dev-FilipeBCS <filipebregaco@gmail.com>
2025-04-28 15:28:11 +0200

    Recover Query params

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,11 @@ const PORT = 3333
 // inicializa o express e armazena em app todas as suas funcionalidades
 const app = express()
 
-app.get("/products/:id/:user", (request, response) => {
-  const { id, user } = request.params
-
-  response.send(`Produto ${id} do usuário ${user}`)
+app.get("/products", (request, response) => {
+  // http://localhost:3333/products/?page=1&limit=10
+  const {page, limit} = request.query
+  
+  response.send(`Página ${page} de ${limit}`)
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
