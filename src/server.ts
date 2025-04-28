fa770102ea92a655e8f9012881c05472e005db4e
dev-FilipeBCS <filipebregaco@gmail.com>
2025-04-28 15:44:29 +0200

    Response with json and status code

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,8 @@ app.get("/products", (request, response) => {
 app.post("/products", (request, response) => {
   const {name, price} = request.body
 
-  response.send(`Produto ${name} custa ${price}`)
+  // response.send(`Produto ${name} custa ${price}`)
+  response.status(201).json({name, price})
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
