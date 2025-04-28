720634d5d822c64f132822b2a105d81af1d6ddf0
dev-FilipeBCS <filipebregaco@gmail.com>
2025-04-28 15:21:55 +0200

    Using Route Params

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ const PORT = 3333
 // inicializa o express e armazena em app todas as suas funcionalidades
 const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello world Express!")
+app.get("/products/:id/:user", (request, response) => {
+  const { id, user } = request.params
+
+  response.send(`Produto ${id} do usuário ${user}`)
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
