1fdcffce3c5b8e39944f33157b75d39c539cc745
dev-FilipeBCS <filipebregaco@gmail.com>
2025-04-28 15:43:37 +0200

    Data in body of request

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,19 @@ const PORT = 3333
 
 // inicializa o express e armazena em app todas as suas funcionalidades
 const app = express()
+app.use(express.json())
 
 app.get("/products", (request, response) => {
   // http://localhost:3333/products/?page=1&limit=10
   const {page, limit} = request.query
   
   response.send(`Página ${page} de ${limit}`)
+})
+
+app.post("/products", (request, response) => {
+  const {name, price} = request.body
+
+  response.send(`Produto ${name} custa ${price}`)
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
